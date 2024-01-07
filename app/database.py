b6b02ff79e2638578a16b58a970e7cc87295298b
HliasOuzounis <iliouzoun@outlook.com>
2024-01-07 17:35:27 +0200

    see available seats

--- app/database.py
+++ app/database.py
@@ -21,7 +21,17 @@ def get_all_flights(depart_city, arrival_city, depart_day):
 	""", (departure_town_code, arrival_town_code, depart_day)).fetchall()
 
 def get_available_seats(flight_id):
-    pass
+    return cur.execute("""
+        select * 
+        from seat
+        where Flight_code = ?
+        and seat.number not in (
+            select Seat_number from purchases 
+            where Flight_code = ? 
+            and Ticket_code not in (select Ticket_code from Cancels)
+        )
+    """, (flight_id, flight_id)).fetchall()
 
 
-print(get_all_flights('New York', 'Miami', '2024-01-06'))
+print(flight_id := get_all_flights('New York', 'Miami', '2024-01-06')[0][0])
+print(seats := get_available_seats(flight_id))
